c07c602d02f7fe419e5a4849c9c64e490c0e645a
Anthony <anthonytran.aht@gmail.com>
2023-10-05 15:46:48 -0400

    Initial commit with basic home page

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PharmaceuticalPortal from './PharmaceuticalPortal';
+
+function App() {
+  return (
+    <div className="App">
+      <PharmaceuticalPortal />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/PharmaceuticalPortal.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './PharmaceuticalPortal.css';
+
+const PharmaceuticalPortal = () => {
+  return (
+    <div className="pharma-container">
+      <main className="content center-align">
+        <h1 className="header-text">Pharmaceutical Portal</h1>
+        <p className="sub-text">Discover a community that supports you in your pharmaceutical journey. Connect, share, and learn together.</p>
+        <button className="get-help-button btn">GET HELP</button>
+      </main>
+    </div>
+  );
+}
+
+export default PharmaceuticalPortal;
