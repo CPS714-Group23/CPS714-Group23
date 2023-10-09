65650a1599f3b8d6b28aff50f25ad13b89e4f7d9
Kish322 <kyogeswaran@ryerson.ca>
2023-10-09 17:11:58 -0400

    create navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import PharmaceuticalPortal from './PharmaceuticalPortal';
+import Navbar from './components/Nav/Nav';
 
 function App() {
   return (
     <div className="App">
+      <Navbar /> 
       <PharmaceuticalPortal />
     </div>
   );
