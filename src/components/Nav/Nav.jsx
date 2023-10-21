df565f46df7b3bba12e7cef14911864366a58492
Gurveer-C1331 <45864502+Gurveer-C1331@users.noreply.github.com>
2023-10-21 16:45:26 -0400

    Implemented Nav and Home page
    -only lg viewport

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,85 +1,48 @@
 import React from 'react';
-import { AppBar, Toolbar, Button } from '@mui/material';
+import { Button } from '@mui/material';
 import { Link, useLocation } from 'react-router-dom';
+import { ReactComponent as Logo } from './logo.svg';
 import './Nav.css';
 
 function Navbar() {
-  const buttonStyle = {
-    fontSize: '20px',
-    fontFamily: 'Raleway, sans-serif',
-  };
-
   const location = useLocation();
+  const menu = [
+    { name: 'Services', path: '/services'},
+    { name: 'About', path: '/about'}
+  ];
+  const menuButtons = menu.map(item =>
+    <Button component={Link} to={item.path}
+      style={{
+        fontWeight: location.pathname === item.path ? '600' : '500'
+      }}>
+      {item.name}
+    </Button>
+  )
 
   return (
-    <AppBar position="sticky" className="navbar">
-      <Toolbar className="toolbar">
-        <div className="left-nav">
-          <Button
-            color="inherit"
-            component={Link}
-            to="/"
-            style={{
-              ...buttonStyle,
-              fontWeight: location.pathname === '/' ? 'bold' : 'normal',
-              opacity: location.pathname === '/' ? 1 : 0.7,
-            }}
-          >
-            Home
-          </Button>
-          <Button
-            color="inherit"
-            component={Link}
-            to="/contact"
-            style={{
-              ...buttonStyle,
-              fontWeight: location.pathname === '/contact' ? 'bold' : 'normal',
-              opacity: location.pathname === '/contact' ? 1 : 0.7,
-            }}
-          >
-            Contact
-          </Button>
-          <Button
-            color="inherit"
-            component={Link}
-            to="/about"
-            style={{
-              ...buttonStyle,
-              fontWeight: location.pathname === '/about' ? 'bold' : 'normal',
-              opacity: location.pathname === '/about' ? 1 : 0.7,
-            }}
-          >
-            About
-          </Button>
-        </div>
-        <div className="right-nav" style={{ display: 'flex', alignItems: 'center' }}>
-          <Button
-            color="inherit"
-            component={Link}
-            to="/signup"
-            style={{
-              ...buttonStyle,
-              fontWeight: location.pathname === '/signup' ? 'bold' : 'normal',
-              opacity: location.pathname === '/signup' ? 1 : 0.7,
-            }}
-          >
-            SignUp
-          </Button>
-          <Button
-            color="inherit"
-            component={Link}
-            to="/login"
-            style={{
-              ...buttonStyle,
-              fontWeight: location.pathname === '/login' ? 'bold' : 'normal',
-              opacity: location.pathname === '/login' ? 1 : 0.7,
-            }}
-          >
-            Login
-          </Button>
-        </div>
-      </Toolbar>
-    </AppBar>
+    <div className="navbar">
+      <div className="left-nav">
+        <Logo />
+        <Button component={Link} to="/" id='logoButton'>
+          Pharmaceutical Portal
+        </Button>
+      </div>
+      <div className="right-nav" style={{ display: 'flex', alignItems: 'center' }}>
+        {menuButtons}
+        <Button component={Link} to="/login" id='logInButton'
+          style={{
+            fontWeight: location.pathname === '/login' ? '600' : '500'
+          }}>
+          Log In
+        </Button>
+        <Button component={Link} to="/signup" id='registerButton'
+          style={{
+            fontWeight: location.pathname === '/signup' ? '600' : '500'
+          }}>
+          Register
+        </Button>
+      </div>
+    </div>
   );
 }
 
--- src/pages/home/PharmaceuticalPortal.jsx
+++ src/pages/home/PharmaceuticalPortal.jsx
@@ -1,15 +1,24 @@
 import React from 'react';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+import { Button } from '@mui/material';
+import { Link } from 'react-router-dom';
 import './PharmaceuticalPortal.css';
 
 const PharmaceuticalPortal = () => {
   return (
-    <div className="pharma-container">
-      <main className="content center-align">
-        <h1 className="header-text">Pharmaceutical Portal</h1>
-        <p className="sub-text">Discover a community that supports you in your pharmaceutical journey. Connect, share, and learn together.</p>
-        <button className="get-help-button btn">GET HELP</button>
-      </main>
-    </div>
+    <Grid container spacing={8} className='pharma-container'>
+      <Grid item lg={5} className='content'>
+          <h1 className="message-text">Discover a community that supports you in your pharmaceutical journey.</h1>
+          <p className="slogan-text">Connect. Share. Learn Together.</p>         
+          <Button component={Link} to="/login" id='getHelpButton'>
+            Get Help Today
+          </Button>
+      </Grid>
+      <Grid item lg={7}>
+        <Paper elevation={1} className='image-container'/>
+      </Grid>
+    </Grid>
   );
 }
 
