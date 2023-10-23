c6e4d6344d7a4eb816bfccc4f1fd2ebd6a2187cd
Gurveer-C1331 <45864502+Gurveer-C1331@users.noreply.github.com>
2023-10-23 13:36:44 -0400

    Updated Nav bar and Home page
    -nav bar and home page are responsive

--- src/pages/home/PharmaceuticalPortal.jsx
+++ src/pages/home/PharmaceuticalPortal.jsx
@@ -9,14 +9,16 @@ const PharmaceuticalPortal = () => {
   return (
     <Grid container spacing={8} className='pharma-container'>
       <Grid item lg={5} className='content'>
-          <h1 className="message-text">Discover a community that supports you in your pharmaceutical journey.</h1>
-          <p className="slogan-text">Connect. Share. Learn Together.</p>         
+          <div className='text-container'>
+            <h1 className="message-text">Discover a community that supports you in your pharmaceutical journey.</h1>
+            <p className="slogan-text">Connect. Share. Learn Together.</p>         
+          </div>
           <Button component={Link} to="/signup" id='getHelpButton'>
             Get Help Today
           </Button>
       </Grid>
-      <Grid item lg={7}>
-        <Paper elevation={1} className='image-container'/>
+      <Grid item lg={7} className='image-container'>
+        <Paper elevation={1} className='image'/>
       </Grid>
     </Grid>
   );
