6f15fed4eed16760964c78f6125461e5983d8cd9
Muhammad3921 <apnahasnain@gmail.com>
2023-11-06 21:10:56 -0500

    Added code to connect to psg database
    (doesnt work yet)

--- Node-Backend/Server/index.js
+++ Node-Backend/Server/index.js
@@ -5,8 +5,34 @@ const bodyParser = require("body-parser");
 const signupRoute = require("../routes/signup"); 
 
 const PORT = process.env.PORT || 3001;
-
+const { Client } = require("pg");
+const dotenv = require("dotenv");
+dotenv.config();
 const app = express();
+
+//database config
+const connectDb = async () => {
+  try {
+      const client = new Client({
+          user: process.env.PGUSER,
+          host: process.env.PGHOST,
+          database: process.env.PGDATABASE,
+          password: process.env.PGPASSWORD,
+          port: process.env.PGPORT,
+          ssl: true
+      })
+
+      await client.connect()
+      const res = await client.query('SELECT * FROM some_table')
+      console.log(res)
+      await client.end()
+  } catch (error) {
+      console.log(error)
+  }
+}
+
+//connecting
+connectDb()
 
 app.use(bodyParser.json());
 
