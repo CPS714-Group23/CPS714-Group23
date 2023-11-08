98b43d599c2c5bfcdfea21d700028694c5ced852
Anthony <anthonytran.aht@gmail.com>
2023-11-08 15:29:05 -0500

    Adding modify and remove events from calendar

--- Frontend/src/pages/scheduler/scheduler.jsx
+++ Frontend/src/pages/scheduler/scheduler.jsx
@@ -41,38 +41,100 @@ function Scheduler() {
 
   const addEvent = async (newEvent) => {
     try {
-      const response = await fetch('/api/scheduler/add', {
-        method: 'POST',
+      const response = await fetch("/api/scheduler/add", {
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
         body: JSON.stringify(newEvent),
       });
-      
+
       if (response.ok) {
         const addedEvent = await response.json();
-        // Add the new event to the current state
-        setEvents(currentEvents => [...currentEvents, addedEvent.event]);
+        setEvents((currentEvents) => [...currentEvents, addedEvent.event]);
       } else {
-        throw new Error('Network response was not ok.');
+        throw new Error("Network response was not ok.");
       }
     } catch (error) {
-      console.error('There has been a problem with your fetch operation:', error);
+      console.error(
+        "There has been a problem with your fetch operation:",
+        error
+      );
+    }
+  };
+
+  const removeEvent = async (eventTitle) => {
+    try {
+      const response = await fetch(
+        `/api/scheduler/remove/${encodeURIComponent(eventTitle)}`,
+        {
+          method: "DELETE",
+        }
+      );
+      if (response.ok) {
+        const result = await response.json();
+        console.log("Event removed:", result);
+        setEvents((currentEvents) =>
+          currentEvents.filter((event) => event.title !== eventTitle)
+        );
+      } else {
+        console.error("Failed to remove the event");
+      }
+    } catch (error) {
+      console.error(
+        "There has been a problem with your fetch operation:",
+        error
+      );
+    }
+  };
+
+  const updateEvent = async (eventTitle, updatedEventData) => {
+    try {
+      const response = await fetch(
+        `/api/scheduler/update/${encodeURIComponent(eventTitle)}`,
+        {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(updatedEventData),
+        }
+      );
+      if (response.ok) {
+        const result = await response.json();
+        console.log("Event updated:", result);
+        setEvents((currentEvents) =>
+          currentEvents.map((event) =>
+            event.title === eventTitle
+              ? { ...event, ...updatedEventData }
+              : event
+          )
+        );
+      } else {
+        console.error("Failed to update the event");
+      }
+    } catch (error) {
+      console.error(
+        "There has been a problem with your fetch operation:",
+        error
+      );
     }
   };
 
   function renderEventContent(eventInfo) {
-    // Check if the event is of type "Appointments"
     if (eventInfo.event.extendedProps.type === "Appointments") {
-      let startTime = new Date(eventInfo.event.start).toLocaleTimeString('en-US', {
-        hour: 'numeric',
-        minute: '2-digit'
+      let startTime = new Date(eventInfo.event.start).toLocaleTimeString(
+        "en-US",
+        {
+          hour: "numeric",
+          minute: "2-digit",
+        }
+      );
+      let endTime = new Date(eventInfo.event.end).toLocaleTimeString("en-US", {
+        hour: "numeric",
+        minute: "2-digit",
       });
-      let endTime = new Date(eventInfo.event.end).toLocaleTimeString('en-US', {
-        hour: 'numeric',
-        minute: '2-digit'
-      });
-      
+
       return (
         <>
           <div>
@@ -83,14 +145,13 @@ function Scheduler() {
           <div>@ {eventInfo.event.extendedProps.location}</div>
         </>
       );
-    } 
-    // Check if the event is of type "Medications"
+    }
     else if (eventInfo.event.extendedProps.type === "Medications") {
-      let time = new Date(eventInfo.event.start).toLocaleTimeString('en-US', {
-        hour: 'numeric',
-        minute: '2-digit'
+      let time = new Date(eventInfo.event.start).toLocaleTimeString("en-US", {
+        hour: "numeric",
+        minute: "2-digit",
       });
-      
+
       return (
         <>
           <div>
@@ -101,31 +162,53 @@ function Scheduler() {
       );
     }
   }
-      
+
   // function handleDateClick(info) {
   //   console.log("Date selected:", info.dateStr);
   // }
 
-  function handleFormSubmit(event) {
+  const handleAddFormSubmit = async (event) => {
     event.preventDefault();
-    // Get form data and create an event object
     const newEvent = {
       title: event.target.title.value,
-      start: new Date(event.target.start.value),
-      end: new Date(event.target.end.value),
-      extendedProps: {
-        type: event.target.type.value,
-        location: event.target.location.value,
-      },
+      start: event.target.start.value,
+      end: event.target.end.value,
+      type: event.target.type.value,
+      location: event.target.location.value,
     };
-    // Call the addEvent function with the new event object
-    addEvent(newEvent);
-  }
-  
+
+    await addEvent(newEvent);
+  };
+
+  const handleUpdateFormSubmit = async (event) => {
+    event.preventDefault();
+    const eventTitle = event.target.title.value;
+    const updatedEventData = {
+      title: event.target.newTitle ? event.target.newTitle.value : eventTitle,
+      start: event.target.start.value,
+      end: event.target.end.value,
+      type: event.target.type.value,
+      location: event.target.location.value,
+    };
+
+    await updateEvent(eventTitle, updatedEventData);
+  };
+
+  const handleDeleteFormSubmit = async (event) => {
+    event.preventDefault();
+    const eventTitle = event.target.title.value;
+
+    await removeEvent(eventTitle);
+    setEvents((currentEvents) =>
+      currentEvents.filter((event) => event.title !== eventTitle)
+    );
+  };
 
   return (
     <div>
-      <h1><b>Scheduler</b></h1>
+      <h1>
+        <b>Scheduler</b>
+      </h1>
       <FullCalendar
         plugins={[dayGridPlugin]} //, interactionPlugin]}
         // dateClick={handleDateClick}
@@ -137,15 +220,46 @@ function Scheduler() {
       />
 
       {/* Test event function */}
-      <form onSubmit={handleFormSubmit}>
-      <input type="text" name="title" placeholder="Event Title" required />
-      <input type="datetime-local" name="start" placeholder="Start Time" required />
-      <input type="datetime-local" name="end" placeholder="End Time" required />
-      <input type="text" name="type" placeholder="Event Type" required />
-      <input type="text" name="location" placeholder="Location" required />
-      <button type="submit">Add Event</button>
-    </form>
+      <form onSubmit={handleAddFormSubmit}>
+        <input type="text" name="title" placeholder="Event Title" required />
+        <input
+          type="datetime-local"
+          name="start"
+          placeholder="Start Time"
+          required
+        />
+        <input
+          type="datetime-local"
+          name="end"
+          placeholder="End Time"
+          required
+        />
+        <input type="text" name="type" placeholder="Event Type" required />
+        <input type="text" name="location" placeholder="Location" required />
+        <button type="submit">Add Event</button>
+      </form>
 
+      {/* Update Event Form */}
+      <form onSubmit={handleUpdateFormSubmit}>
+        <input
+          type="text"
+          name="title"
+          placeholder="Current Event Title"
+          required
+        />
+        <input type="text" name="newTitle" placeholder="New Event Title" />
+        <input type="datetime-local" name="start" placeholder="Start Time" />
+        <input type="datetime-local" name="end" placeholder="End Time" />
+        <input type="text" name="type" placeholder="Event Type" />
+        <input type="text" name="location" placeholder="Location" />
+        <button type="submit">Update Event</button>
+      </form>
+
+      {/* Delete Event Form */}
+      <form onSubmit={handleDeleteFormSubmit}>
+        <input type="text" name="title" placeholder="Event Title" required />
+        <button type="submit">Delete Event</button>
+      </form>
     </div>
   );
 }
--- Node-Backend/routes/scheduler.js
+++ Node-Backend/routes/scheduler.js
@@ -1,9 +1,28 @@
 const express = require('express');
 const router = express.Router();
 
+// mock database storage
+let events = [];
+
 async function insertEventToDatabase(eventData) {
-  console.log('Inserting event into the database:', eventData);
-  return { id: Date.now(), ...eventData }; // simulate an auto-generated ID
+  const newEvent = { id: eventData.title, ...eventData };
+  events.push(newEvent);
+  return newEvent;
+}
+
+async function removeEventFromDatabase(eventId) {
+  events = events.filter(event => event.id !== eventId);
+  return { id: eventId };
+}
+
+async function updateEventInDatabase(eventId, updatedEventData) {
+  events = events.map(event => {
+    if (event.id === eventId) {
+      return { ...event, ...updatedEventData };
+    }
+    return event;
+  });
+  return { id: eventId, ...updatedEventData };
 }
 
 router.post('/add', async (req, res) => {
@@ -18,6 +37,29 @@ router.post('/add', async (req, res) => {
   }
 });
 
-// define the '/remove' and '/edit' routes
-
+router.delete('/remove/:id', async (req, res) => {
+    try {
+      const eventId = req.params.id;
+      const removedEvent = await removeEventFromDatabase(eventId);
+  
+      res.json({ message: "Event removed successfully", event: removedEvent });
+    } catch (error) {
+      console.error('Error removing event:', error);
+      res.status(500).json({ message: "Error removing event", error: error.message });
+    }
+  });
+  
+  router.put('/update/:id', async (req, res) => {
+    try {
+      const eventId = req.params.id;
+      const eventData = req.body;
+      const updatedEvent = await updateEventInDatabase(eventId, eventData);
+  
+      res.json({ message: "Event updated successfully", event: updatedEvent });
+    } catch (error) {
+      console.error('Error updating event:', error);
+      res.status(500).json({ message: "Error updating event", error: error.message });
+    }
+  });
+  
 module.exports = router;
