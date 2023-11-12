0bcdb9390ff9afc8dbc38220443cc24e82f03daa
tariqek11 <tarique.n.khan@torontomu.ca>
2023-11-12 15:35:47 -0500

    Implemented Twilio Function

--- Node-Backend/Server/index.js
+++ Node-Backend/Server/index.js
@@ -4,6 +4,8 @@ const signupRoute = require("../routes/signup");
 const loginRoute = require("../routes/login"); 
 const schedulerRoute = require("../routes/scheduler");
 const app = express();
+const client = require('twilio')('AC28416c6be03f3b835b1f7eb6d6088865', '56cc34c4c88edf11b9c12f2f350c1b51'); //need account SID and token
+
 require('dotenv').config();
 
 const PORT = process.env.PORT || 3001;
@@ -21,3 +23,13 @@ app.use("/api/scheduler", schedulerRoute);
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
+
+function sendTextMessage(msg){
+  client.messages.create({
+      body: msg,
+      to: '+16479396641',
+      from: '+12015814244'
+  }).then(message => console.log(message))
+  // implement fallback code
+  .catch(error => console.log(error))
+}
