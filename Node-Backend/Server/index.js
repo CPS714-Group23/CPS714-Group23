4380725a4d97c3ca4562a119c0c94a638229d023
ShawnNimal22 <147085822+ShawnNimal22@users.noreply.github.com>
2023-11-14 12:23:06 -0500

    Email index.js

--- Node-Backend/Server/index.js
+++ Node-Backend/Server/index.js
@@ -19,3 +19,36 @@ app.use("/signup", signupRoute);
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
+
+
+const nodemailer = require('nodemailer');
+
+async function sendEmail(to, subject, text) {
+    // Create a transporter
+    let transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: 'shawnb.nimal22@gmail.com', // Replace with your Gmail address
+            pass: 'dsph kztb wnab aspw' // Replace with your app-specific password
+        }
+    });
+
+    // Set email options
+    let mailOptions = {
+        from: 'shawnb.nimal22@gmail.com', // Replace with your Gmail address
+        to: to, // Recipient email
+        subject: subject, // Email subject
+        text: text, // Email body text
+    };
+
+    // Send email
+    try {
+        let info = await transporter.sendMail(mailOptions);
+        console.log('Message sent: %s', info.messageId);
+    } catch (error) {
+        console.error('Error sending email:', error);
+    }
+}
+
+// Example usage
+sendEmail('shawn.nimal@gmail.com', 'Hello', 'Hello from Node.js!');
